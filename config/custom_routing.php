c09fd700acf0828d468946c7f76dd12be20f4b62
Reconix <Reconix@users.noreply.github.com>
2023-08-31 14:46:51 +0100

    Include Custom Routing from Modules
    Also missed out the pages route

--- config/custom_routing.php
+++ config/custom_routing.php
@@ -1,11 +1,21 @@
 <?php
 $routes = [
     'tg-admin' => 'trongate_administrators/login',
-    'tg-admin/submit_login' => 'trongate_administrators/submit_login',
-
-    //
-    'admin' => 'trongate_administrators/login',
-    'admin/manage' => 'trongate_administrators/manage'
-
+    'trongate-pages' => 'trongate_pages/index',
+    'tg-admin/submit_login' => 'trongate_administrators/submit_login'
 ];
+
+// Include custom routes from modules.
+foreach (glob($dir . 'modules/*/*_routes.php') as $file) {
+
+    $moduleName = basename(dirname($file));
+
+    include_once $file;
+
+    // Merge the routes
+    if (isset(${$moduleName . '_routes'})) {
+        $routes = array_merge($routes, ${$moduleName . '_routes'});
+    }
+}
+
 define('CUSTOM_ROUTES', $routes);
